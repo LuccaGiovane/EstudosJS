965e8ef35a0f3ef0533ba3005c10bff7dfe1855f
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2023-11-30 14:44:02 -0300

    Projeto 2: - Projeto Finalizado - três temporizadores adicionados e funcionando.

--- Prj 02 - Pomodoro/script.js
+++ Prj 02 - Pomodoro/script.js
@@ -5,11 +5,22 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const simboloPausePlay = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
+
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 
+
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
 musica.loop = true;
-musicaFocoInput.addEventListener('change', () => {
+musicaFocoInput.addEventListener('change', () =>
+{
     if(musica.paused)
     {
         musica.play();
@@ -20,33 +31,36 @@ musicaFocoInput.addEventListener('change', () => {
     }
 })
 
-focoBt.addEventListener('click', () =>
-{
-    alterarContexto('foco');
-    focoBt.classList.add('active');
-
-})
-
-curtoBt.addEventListener('click', () =>
-{
-    alterarContexto('descanso-curto');
-    curtoBt.classList.add('active');
-})
-
-longoBt.addEventListener('click', () =>
-{
-   alterarContexto('descanso-longo');
-    longoBt.classList.add('active');
+focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
+    alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 
+curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
+})
 
-function alterarContexto(contexto)
-{
-    botoes.forEach(function (contexto)
-    {
-        contexto.classList.remove('active');
+
+longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
+})
+
+
+
+
+function alterarContexto(contexto) {
+    mostrarTempo()
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
     })
+
+
 
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `imagens/${contexto}.png`)
@@ -72,3 +86,47 @@ function alterarContexto(contexto)
             break;
     }
 }
+
+const contagemRegressiva = () =>
+{
+    if(tempoDecorridoEmSegundos <= 0)
+    {
+        alert('Tempo finalizado!');
+        zerar();
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar()
+{
+    if(intervaloId)
+    {
+        zerar();
+        return;
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausarBt.textContent = "Pausar";
+    simboloPausePlay.setAttribute('src', `imagens/pause.png`);
+}
+
+function zerar()
+{
+    clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "Iniciar";
+    simboloPausePlay.setAttribute('src', `imagens/play_arrow.png`);
+    intervaloId = null;
+}
+
+function mostrarTempo()
+{
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+
+mostrarTempo();
