860dd70b1d12e02c927bdd6e3e7a82370646148d
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2023-12-13 08:40:49 -0300

    Projeto 3: - ReconhecimentoDeVoz.js: utliza a API de reconhecimento de voz para saber o número falado e o mostra na tela.
    -SortearNumero.js: Faz o sorteio do numero para o jogo

---
+++ Prj 03 - Jogo Comando de voz/App/ReconhecimentoDeVoz.js
@@ -0,0 +1,24 @@
+window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+const elementoChute = document.getElementById('chute');
+
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e)
+{
+    chute = e.results[0][0].transcript;
+    exibeChuteNaTela(chute);
+}
+
+function exibeChuteNaTela(chute)
+{
+    elementoChute.innerHTML =
+        `
+        <div>Você disse</div>
+        <span class=”box”>${chute}</span>
+        `
+}
---
+++ Prj 03 - Jogo Comando de voz/App/SortearNumero.js
@@ -0,0 +1,17 @@
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gerarNumeroAleatorio();
+
+const elementoMenorValor = document.getElementById('menor-valor');
+const elementoMaiorValor = document.getElementById('maior-valor');
+
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
+
+
+function gerarNumeroAleatorio()
+{
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
